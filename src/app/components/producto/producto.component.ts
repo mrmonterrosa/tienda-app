9ed0d7b88b155f1555bcf41406972174114fbceb
Carlos Monterrosa <cgonzalezmonterrosa@gmail.com>
2020-12-14 16:17:42 -0500

    MrMonterrosa
    Se corrienron algunos estilos.
    Se agrego el cargando en la tabla.
    Se agrego el mensaje cuando no hay productos.
    Se agrego la captura de errores en las respustas y se agrego un mensaje en la vista cuando hay error.

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -14,6 +14,8 @@ export class ProductoComponent implements OnInit {
 
   productos : ProductoModel[] = [];
   producto: ProductoModel;
+  loading : boolean = true;
+  errorMessage : string = null;
 
   constructor(private productoService : ProductoService) {
     this.clear();
@@ -24,8 +26,14 @@ export class ProductoComponent implements OnInit {
   }
 
   getProductos() {
+    this.loading = true;
     this.productoService.getProductos().subscribe(data => {
       this.productos = data;
+      this.loading = false;
+    }, (erro) => {
+      console.log(erro);
+      this.loading = false;
+      this.errorMessage = `Ocurrio un error: ${ erro.status }`;
     });
   }
 
@@ -34,16 +42,23 @@ export class ProductoComponent implements OnInit {
     if(f.valid) {
       if(this.producto.id == null) {
         this.productoService.createProducto(this.producto).subscribe(data => {
-          console.log("Crear",data);
+          //console.log("Crear",data);
           this.clear();
           this.getProductos();
+        }, (erro) => {
+          console.log(erro);
+          this.loading = false;
+          this.errorMessage = `Ocurrio un error: ${ erro.status }`;
         });
         return;
       }
 
       this.productoService.modificarProducto(this.producto).subscribe(data =>{
-        console.log("Modificar", data);
+        //console.log("Modificar", data);
         this.clear();
+      }, (erro) => {
+        console.log(erro);
+        this.errorMessage = `Ocurrio un error: ${ erro.status }`;
       });
     }
 
@@ -51,13 +66,17 @@ export class ProductoComponent implements OnInit {
 
   eliminar() {
     this.productoService.eliminarProducto(this.producto.id).subscribe(data => {
-      console.log(data);
+      //console.log(data);
       this.getProductos();
+    }, (erro) => {
+      console.log(erro);
+      this.errorMessage = `Ocurrio un error: ${ erro.status }`;
     })
   }
 
   clear() {
     this.producto = new ProductoModel();
+    this.errorMessage = null;
   }
 
   cargaData(producto: ProductoModel) {
